1978efbf2d8b760db38a85e4a1de35be574bc6aa
Sergey Shapovalov <ssv@fozzy.com>
2024-06-05 09:29:26 +0200

    Added validates to bulletins

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,3 +1,7 @@
 class Bulletin < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 1000 }
+  validates :image, attached: true, content_type: %i[png jpg jpeg], size: { less_than: 5.megabytes }
 end
