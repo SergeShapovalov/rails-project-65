8c0824e42f372bec3e59620c14089ba2c26f73e5
Sergey Shapovalov <ssv@fozzy.com>
2024-06-06 14:48:10 +0200

    Added bulletins controller paths

--- app/controllers/web/bulletins_controller.rb
+++ app/controllers/web/bulletins_controller.rb
@@ -7,5 +7,26 @@ module Web
     def new
       @bulletin = Bulletin.new
     end
+
+    def show
+      @bulletin = Bulletin.find(params[:id])
+    end
+
+    def create
+      @bulletin = current_user.bulletins.build(bulletin_params)
+
+      if @bulletin.save
+        redirect_to bulletin_path @bulletin, notice: 'Successfully created bulletin.'
+      else
+        flash.now[:failure] = 'Failed to create bulletin.'
+        render :new
+      end
+    end
+
+    private
+
+    def bulletin_params
+      params.require(:bulletin).permit(:title, :description, :category_id, :image)
+    end
   end
 end
