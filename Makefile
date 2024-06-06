120133165d427fed59085addd6dd8e2f10ddf2bd
Sergey Shapovalov <ssv@fozzy.com>
2024-06-06 18:05:05 +0200

    Fixed auth check

--- Makefile
+++ Makefile
@@ -20,5 +20,4 @@ slim-lint:
 run:
 	bundle exec rails s
 
-
 .PHONY: test
--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,5 +1,6 @@
 module AuthHelper
   def authorized?
+    return false unless session[:user_id]
     auth_user ||= User.find(session[:user_id])
     auth_user.present?
   end
