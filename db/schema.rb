a4f6c7da0777bdcad798ef2b91d9cbe6a97d9d6c
Sergey Shapovalov <ssv@fozzy.com>
2024-06-06 21:50:47 +0200

    Try to fix deploy

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_081211) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bulletins", "categories"
-  add_foreign_key "bulletins", "users"
+  add_foreign_key "bulletin", "categories"
+  add_foreign_key "bulletin", "users"
 end
